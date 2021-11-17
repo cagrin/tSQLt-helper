09100d400535b65520d6e8aa88c07c31c724a0f3
Krzysztof Strugiński <cagrin@gmail.com>
2021-11-18 00:10:32 +0100

    First impl in tSQLt style

--- test/testHelperTurnIntoDataRow.sql
+++ test/testHelperTurnIntoDataRow.sql
@@ -1,6 +1,10 @@
+drop procedure tSQLt.testHelperTurnIntoDataRow;
+go
 drop table stt.invoice;
 go
 drop schema stt;
+go
+drop table ##rows;
 go
 
 create schema stt;
@@ -9,43 +13,44 @@ create table stt.invoice
 (
     inv_id int not null,
     inv_type char(3) not null,
-    inv_cust_id varchar(100) not null,
+    inv_cust_id varchar(100) null,
     inv_amount money not null,
     inv_error nvarchar(max) null
 );
 go
-insert into stt.invoice
-    (inv_id, inv_type, inv_cust_id, inv_amount, inv_error)
-values
+create procedure tSQLt.testHelperTurnIntoDataRow
+as
+begin
+--- Arrange
+	insert into stt.invoice (inv_id, inv_type, inv_cust_id, inv_amount, inv_error) values
     (1, 'FV', 'ABCDE12345', 100.00, NULL),
     (2, 'FV1', 'Zazółć gęślą jaźn', -1.00, NULL),
     (3, 'FV2', 'qwerty asdfgh zxcvb', 1234.56, '?'),
     (4, 'A', 'ABCDE12345', 0.00, 'An error occurred');
-go
 
-declare @max int = (select count(1) from stt.invoice);
-declare @idx int = 0
-declare @schema_name varchar(100) = 'stt'
-declare @table_name varchar(100) = 'invoice'
-declare @column_name varchar(100);
-declare @command nvarchar(max) = 'select ''''''''+'
-while @idx < @max
-begin
-    set @idx = @idx + 1;
+--- Act
+	declare @actual nvarchar(max);
 
-    select @column_name = COLUMN_NAME
-    from INFORMATION_SCHEMA.COLUMNS
-    where TABLE_SCHEMA = 'stt'
-    and TABLE_NAME = 'invoice'
-    and ORDINAL_POSITION = @Idx
+	exec tSQLt.Helper_TurnIntoDataRow
+		@TableName = 'stt.invoice',
+		@Query = 'select inv_id, inv_type, inv_cust_id, inv_amount, inv_error from stt.invoice',
+		@Result = @actual output;
 
-    set @command = @command +  ' '''' + convert(varchar, ' + @column_name + ') + '
+--- Assert
+	declare @expected nvarchar(max) = 'insert into stt.invoice (inv_id, inv_type, inv_cust_id, inv_amount, inv_error) values
+    (1, ''FV '',          ''ABCDE12345'',  100.00,                NULL),
+    (2, ''FV1'',   ''Zazólc gesla jazn'',   -1.00,                NULL),
+    (3, ''FV2'', ''qwerty asdfgh zxcvb'', 1234.56,                 ''?''),
+    (4, ''A  '',          ''ABCDE12345'',    0.00, ''An error occurred'');';
 
-    if @idx < @max
+    print @actual
+
+	if not((@expected = @actual) or (@actual is null and @expected is null))
     begin
-        set @command = @command +  ' '''''', '''''' +'
+        raiserror('tSQLt.testHelperTurnIntoDataRow', 16, 10);
     end
-end
-set @command = @command +  ' '''''''' from ' + @schema_name + '.' + @table_name
-exec sp_executesql @command
+
+end;
+go
+exec tSQLt.testHelperTurnIntoDataRow;
 go
