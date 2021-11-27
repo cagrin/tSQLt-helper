991ba618f8a5a4de754d899b2a92c2288dd1d71d
Krzysztof Strugiński <cagrin@gmail.com>
2021-11-27 20:46:44 +0100

    Organize by unit test class

--- Tests/UnitTests/testConvertIntoInserts.sql
+++ Tests/UnitTests/testConvertIntoInserts.sql
@@ -1,3 +1,19 @@
+create schema testConvertIntoInserts;
+go
+
+create schema stt;
+go
+create table stt.invoice
+(
+    inv_id int not null,
+    inv_type char(3) not null,
+    inv_cust_id varchar(100) null,
+    inv_amount money not null,
+    inv_date date null,
+    inv_error nvarchar(max) null
+);
+go
+
 create procedure testConvertIntoInserts.test1
 as
 begin
