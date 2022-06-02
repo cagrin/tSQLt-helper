c37a3a5624d339b3e5102f99d81eae2bf2dfe86d
Krzysztof Strugiński <cagrin@gmail.com>
2022-06-02 17:41:02 +0200

    Fix collation problem

--- latest/Tests/UnitTests/testConvertIntoInserts.sql
+++ latest/Tests/UnitTests/testConvertIntoInserts.sql
@@ -30,11 +30,15 @@ BEGIN
 		@Result = @Actual OUTPUT;
 
 --- Assert
-	DECLARE @Expected NVARCHAR(MAX) = 'INSERT INTO dbo.invoice (inv_id, inv_type, inv_cust_id, inv_amount, inv_date, inv_error) VALUES
-    (1, ''FV '',          ''ABCDE12345'',  100.00, ''2021-11-27'', NULL),
-    (2, ''FV1'',   ''Zażółć gęślą jaźń'',   -1.00, ''2021-11-28'', NULL),
-    (3, ''FV2'', ''qwerty asdfgh zxcvb'', 1234.56, ''2021-11-29'', ''?''),
-    (4, ''A  '',          ''ABCDE12345'',    0.00,         NULL, ''An ERROR occurred'');';
+	DECLARE @Expected NVARCHAR(MAX) = CONCAT_WS
+    (
+        NCHAR(10),
+        'INSERT INTO dbo.invoice (inv_id, inv_type, inv_cust_id, inv_amount, inv_date, inv_error) VALUES',
+        '    (1, ''FV '',          ''ABCDE12345'',  100.00, ''2021-11-27'', NULL),',
+        '    (2, ''FV1'',   ''Zażółć gęślą jaźń'',   -1.00, ''2021-11-28'', NULL),',
+        '    (3, ''FV2'', ''qwerty asdfgh zxcvb'', 1234.56, ''2021-11-29'', ''?''),',
+        '    (4, ''A  '',          ''ABCDE12345'',    0.00,         NULL, ''An ERROR occurred'');'
+    );
 
     EXEC tSQLt.AssertEqualsString @Expected, @Actual;
 END;
